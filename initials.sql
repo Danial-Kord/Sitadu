1878d44ac174b074764701d602bf13a26fdf4fc9
Danial <danial.kordmodanlou@gmail.com>
2020-01-29 17:32:01 +0330

    logs for insertion ,deletation and updates added ,some changes in tables

---
+++ initials.sql
@@ -0,0 +1,17 @@
+insert into address values ("c","d","e",213);
+insert into customer values ("danial","km","ddan","kord","201531",121351,12,"c");
+
+insert into  address(name,house_phone_number,address) values ("Dsadsa",13512,"dsadadafdaf");
+
+select * from customer;
+insert into customer values ("duser", "pass", "first_name", "last_name", "melli_code", "phone", 22) ;
+insert into factor(id, name, time) values (3313,null,null);
+
+select * from log;
+select *
+from menu_factor;
+
+insert into menu values (21,"dsda",2);
+
+insert into menu_factor values (212,3313,21,"dada",31);
+delete from menu where id = 21;
--- tables.sql
+++ tables.sql
@@ -17,6 +17,8 @@ create table customer
     melli_code char(10) not null,
     phone      char(7),
     age        int(3),
+    check ( melli_code like '%[^0-9]%'),
+    check ( phone like '%[^0-9]%'),
     primary key (user)
 );
 
@@ -27,6 +29,7 @@ create table address(
     address text,
     house_phone_number char(7),
     customer_id varchar(20) ,
+    check ( house_phone_number like '%[^0-9]%'),
     foreign key (customer_id) references customer(user) on delete cascade,
     primary key (id)
 );
@@ -35,14 +38,15 @@ create table peyk(
     id int auto_increment,
     first_name varchar(30) not null,
     last_name  varchar(30) not null,
-    phone     int(7),
+    phone     char(7),
+    check ( phone like '%[^0-9]%'),
     primary key (id)
 );
 create table factor(
     id int auto_increment,
     customer_id varchar(20)  null,
     name char(30) default null,
-    time DATETIME,
+    time DATETIME default current_timestamp,
     peyk_id int null,
     foreign key (peyk_id) references peyk(id) on delete set null ,
     primary key (id),
@@ -82,7 +86,121 @@ primary key (id)
 create table log(#TODO
     id  int(30) auto_increment,
     information text,
+    mtable varchar(20),
+    time DATETIME default current_timestamp,
     primary key (id)
 );
 
+CREATE TRIGGER log  after insert ON customer
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to table customer with username :",new.user),"customer");
+       END;
+
+CREATE TRIGGER log2  after insert ON factor
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to factor address with id :",new.id),"factor");
+       END;
+
+CREATE TRIGGER log3  after insert ON address
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to table address with id :",new.id),"address");
+       END;
+
+CREATE TRIGGER log4  after insert ON peyk
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to table peyk with id :",new.id),"peyk");
+       END;
+
+CREATE TRIGGER log5  after insert ON menu
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to table menu with id :",new.id),"menu");
+       END;
+
+CREATE TRIGGER log6  after insert ON raw_material
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("insertion to table raw_material with id :",new.id),"raw_material");
+       END;
+
+
+
+CREATE TRIGGER log11  after delete ON customer
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to table customer with username :",OLD.user),"customer");
+       END;
+
+CREATE TRIGGER log21  after delete ON factor
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to factor address with id :",OLD.id),"factor");
+       END;
+
+CREATE TRIGGER log31  after delete ON address
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to table address with id :",OLD.id),"address");
+       END;
+
+CREATE TRIGGER log41  after delete ON peyk
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to table peyk with id :",OLD.id),"peyk");
+       END;
+
+CREATE TRIGGER log51  after delete ON menu
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to table menu with id :",OLD.id),"menu");
+       END;
+
+CREATE TRIGGER log61  after delete ON raw_material
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("deletation to table raw_material with id :",OLD.id),"raw_material");
+       END;
+
+
+
+
+CREATE TRIGGER log111  after update ON customer
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to table customer with username :",OLD.user,"and new username is :",new.user),"customer");
+       END;
+
+CREATE TRIGGER log211  after update ON factor
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to factor address with id :",OLD.id,"and new id is :",new.id),"factor");
+       END;
+
+CREATE TRIGGER log311  after update ON address
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to table address with id :",OLD.id,"and new id is :",new.id),"address");
+       END;
+
+CREATE TRIGGER log411  after update ON peyk
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to table peyk with id :",OLD.id,"and new id is :",new.id),"peyk");
+       END;
+
+CREATE TRIGGER log511  after update ON menu
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to table menu with id :",OLD.id,"and new id is :",new.id),"menu");
+       END;
+
+CREATE TRIGGER log611  after update ON raw_material
+       FOR EACH ROW
+       BEGIN
+            insert into log(information, `mtable`)  values (concat("update to table raw_material with id :",OLD.id,"and new id is :",new.id),"raw_material");
+       END;
 
